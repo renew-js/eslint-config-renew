8a54a6d0613cf9e856bc0c60c6674e34b3122b39
Marc Richter <marc@richter.bz>
2019-01-15 15:02:28 +0100

    Set no-unused-vars to warn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = {
     rules: {
         'no-console': 0,
         'indent': [2, 4],
-        'no-unused-vars': 0,
-        'space-before-function-paren': [0, 'always'],
+        'no-unused-vars': 1,
+        'space-before-function-paren': [2, 'always'],
         'object-curly-spacing': [0, 'always'],
         'array-bracket-spacing': [0, 'always'],
         'require-jsdoc': [1, {
