e2a3a0b089f61aef95fcad24965a53dc32ac2f2e
Marc Richter <marc@richter.bz>
2019-03-02 23:00:05 +0100

    Force padding on objects, arrays, classes. Fix switch case indent

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
 module.exports = {
     rules: {
         'no-console': 0,
-        'indent': [2, 4],
+        'indent': [2, 4, {SwitchCase: 1}],
         'no-unused-vars': 1,
         'space-before-function-paren': [2, 'always'],
-        'object-curly-spacing': [0, 'always'],
-        'array-bracket-spacing': [0, 'always'],
+        'object-curly-spacing': [2, 'always'],
+        'array-bracket-spacing': [2, 'always'],
         'require-jsdoc': [1, {
             'require': {
                 'FunctionDeclaration': true,
                 'MethodDefinition': true,
                 'ClassDeclaration': true
             }
-        }]
+        }],
+        'padded-blocks': [2, {classes: 'always'}]
     }
 };
